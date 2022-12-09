39c66f957f056fcf4704d8630e2a627da24bb4c0
Juan <projectnoa@users.noreply.github.com>
2022-12-09 11:10:49 -0700

    Initial Commit

---
+++ SwiftUISliderSample/SwiftUISliderSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//	SwiftUISliderSampleApp.swift
+//  SwiftUISliderSample
+//
+//  Created by Juan Mueller on 12/9/22.
+//  For more, visit www.ajourneyforwisdom.com
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISliderSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
