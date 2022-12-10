adaee0c871fc06d60afafa608fe89d7e31bbc365
Juan <projectnoa@users.noreply.github.com>
2022-12-09 17:30:32 -0700

    First Commit

--- SwiftUISliderSample/ContentView.swift
+++ SwiftUISliderSample/ContentView.swift
@@ -8,15 +8,97 @@
 
 import SwiftUI
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
 struct ContentView: View {
+    @State private var contrast: Double = 100
+    @State private var brightness: Double = 0
+    @State private var saturation: Double = 100
+    
+    var filter: CIFilter?
+    
+    @State private var image: Image?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.horizontal, 50)
+                .padding(.bottom, 30)
+            
+            HStack {
+                Text("Contrast:")
+                    .bold()
+                Text("\((Int(contrast) - 100))%")
+                    .bold()
+            }
+            
+            Slider(value: $contrast, in: 0...200, step: 1)
+                .padding(.horizontal, 10)
+                .onChange(of: contrast,
+                          perform: alterImage)
+            
+            HStack {
+                Text("Brightness:")
+                    .bold()
+                Text("\(Int(brightness))%")
+                    .bold()
+            }
+            
+            Slider(value: $brightness, in: -100...100, step: 1)
+                .padding(.horizontal, 10)
+                .onChange(of: brightness,
+                          perform: alterImage)
+            
+            HStack {
+                Text("Saturation:")
+                    .bold()
+                Text("\((Int(saturation) - 100))%")
+                    .bold()
+            }
+            
+            Slider(value: $saturation, in: 0...200, step: 1)
+                .padding(.horizontal, 10)
+                .onChange(of: saturation,
+                          perform: alterImage)
+            
+            Button("Reset") {
+                contrast = 100
+                brightness = 0
+                saturation = 100
+            }.padding(.top, 30)
+                .buttonStyle(.bordered)
+        }.onAppear {
+            image = Image("img")
+        }.padding(10)
+    }
+    
+    func alterImage(by _: Double) {
+        // Retrieve clean instance of the image
+        guard let inputImage = UIImage(named: "img") else { return }
+        // Initialize a Core Image instance with the UIimage
+        let beginImage = CIImage(image: inputImage)
+        // Initialize context
+        let context = CIContext()
+        // Initialize Filter
+        let filter = CIFilter.colorControls()
+        // Set image
+        filter.inputImage = beginImage
+        // Apply filter values
+        filter.contrast = Float(contrast / 100)
+        filter.brightness = Float(brightness / 100)
+        filter.saturation = Float(saturation / 100)
+        // Retrieve the output
+        guard let outputImage = filter.outputImage else { return }
+        // Create a CGImage instance from the output
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            // Get the UIimage equivalent
+            let uiImage = UIImage(cgImage: cgimg)
+            // Update the image instance
+            image = Image(uiImage: uiImage)
         }
-        .padding()
     }
 }
 
